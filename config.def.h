c834bbc6027e6bb7a288843b1d253efeaec5bc1a
Philip Olesen <nb.philip@gmail.com>
2021-05-28 12:39:30 +0200

    smaller font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "iosevka term:pixelsize=14:antialias=true:autohint=true";
+static char *font = "iosevka term:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*
